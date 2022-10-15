3c75102700c0fd14311eb872d763367ee916e8b6
Roman <tsugelroman1998@gmail.com>
2022-10-15 15:51:10 +0300

    first commit

--- JS/task2.js
+++ JS/task2.js
@@ -42,7 +42,7 @@
 
 
 
-// // 3) створити функцію яка приймає три числа та виводить найменьше. (Без Math.min!) 
+// // 3) створити функцію яка приймає три числа та виводить найменьше. (Без Math.min!) ↓
 // function minimal_digit(a,b,c)
 // {
 //     let min = 9999;
@@ -67,7 +67,7 @@
 
 
 
-// // 4) створити функцію яка приймає три числа та виводить найбільше. (Без Math.max!)
+// // 4) створити функцію яка приймає три числа та виводить найбільше. (Без Math.max!) ↓
 // function maximum_digit(a,b,c)
 // {
 //     let max = -9999;
@@ -110,13 +110,13 @@
     
 // }
 
-// let mas = [21,09,2005];
+// const mas = [21,09,2005];
 
 //    maximum_digit(mas);
 
 
 
-// // 6) створити функцію яка повертає найменьше число з масиву
+// // 6) створити функцію яка повертає найменьше число з масиву ↓
 // function minimal_digit(dig)
 // {
 //     let max = -9999;
@@ -134,9 +134,119 @@
     
 // }
 
-// let mas = [21,09,2005];
+// const mas = [21,09,2005];
 
 // minimal_digit(mas);
+
+
+
+// // 7) створити функцію яка приймає масив чисел, сумує значення елементів масиву та повертає його ↓
+// function sum_digits(dig)
+// {
+//     let x = 0;
+
+//     for(let i=0; i<dig.length; i++)
+//     {
+//         x += dig[i];
+//     }
+
+//     return console.log("Sum of all digits is:", x);
+    
+// }
+
+// const mas = [2,7,2,1,5,7,8,9];
+
+// console.log(mas);
+// sum_digits(mas);
+
+
+
+// // 8) створити функцію яка приймає масив чисел та повертає середнє арифметичне його значень ↓
+// function arithmetic_mean(dig)
+// {
+//     let x = 0;
+
+//     for(let i=0; i<dig.length; i++)
+//     {
+//         x += dig[i];
+//     }
+
+//     x /= (dig.length);
+
+//     return console.log("Arithmetic mean of digits is:", x);
+    
+// }
+
+// const mas = [2,7,12,1,5,7,8,9];
+
+// console.log(mas);
+// arithmetic_mean(mas);
+
+
+
+// // 9) Створити функцію яка приймає масив будь яких объектів, та повертає масив ключів всіх обєктів ↓
+// function keys_returning(our_obj)
+// {
+//     return console.log(Object.keys(our_obj));
+// }
+
+// const   list = {
+
+//     item1: 'bread',
+//     item2: 'milk',
+//     item3: 'beer',
+//     item4: 'snacks'
+
+
+// };
+
+
+// keys_returning(list);
+
+
+
+// // 10) Створити функцію яка приймає масив будь яких объектів, та повертає масив значень всіх обєктів ↓
+// function keys_returning(our_obj)
+// {
+//     return console.log(Object.values(our_obj));
+// }
+
+// const   list = {
+
+//     item1: 'bread',
+//     item2: 'milk',
+//     item3: 'beer',
+//     item4: 'snacks'
+
+
+// };
+
+// keys_returning(list);
+
+
+
+
+// 11) створити функцію  яка скаладає значення елементів з однаковими індексами  та повертає новий результуючий масив ↓
+// function sum_index(dig)
+// {
+
+//     for(let i=0; i<dig.length; i++)
+//     {
+//         if()
+//         {
+
+//         }                                                          ??????
+//     }
+
+    
+// }
+
+// const mas = [2,7,12,1,5,7,8,9];
+
+// console.log(mas);
+// sum_index(mas);
+
+
 
 
 
